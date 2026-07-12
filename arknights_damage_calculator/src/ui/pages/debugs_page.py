e19dc0dd991d77f46759ed1428baf5d0740d7de9
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-12 16:41:24 +0900

    Add trait calculator debug page

--- arknights_damage_calculator/src/ui/pages/debugs_page.py
+++ arknights_damage_calculator/src/ui/pages/debugs_page.py
@@ -3,11 +3,17 @@ try:
         render_damage_calculator_debug_page,
     )
     from src.ui.debug_pages.operator_loader_debug import render_debug_operator_loader_page
+    from src.ui.debug_pages.trait_calculator_debug import (
+        render_trait_calculator_debug_page,
+    )
 except ModuleNotFoundError:
     from ui.debug_pages.damage_calculator_debug import (
         render_damage_calculator_debug_page,
     )
     from ui.debug_pages.operator_loader_debug import render_debug_operator_loader_page
+    from ui.debug_pages.trait_calculator_debug import (
+        render_trait_calculator_debug_page,
+    )
 
 
 def render_debugs_page():
@@ -20,6 +26,7 @@ def render_debugs_page():
         [
             "operator_loader.py",
             "damage_calculator.py",
+            "trait_calculator.py",
         ],
     )
 
@@ -28,3 +35,6 @@ def render_debugs_page():
 
     elif debug_page == "damage_calculator.py":
         render_damage_calculator_debug_page()
+
+    elif debug_page == "trait_calculator.py":
+        render_trait_calculator_debug_page()
